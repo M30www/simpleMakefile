d65f783d6f808c1ba74d9ef46cf4ea71c21ea061
meow <qaqmoss@gmail.com>
2023-02-09 19:00:46 +0800

    something about makefile

---
+++ factorial.cpp
@@ -0,0 +1,6 @@
+#include "functions.h"
+
+int factorial(int n) {
+    if (n == 1) return 1;
+    else return n * factorial(n - 1);
+}
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "functions.h"
+
+using namespace std;
+
+int main() {
+    printHello();
+    cout << "this is main :" << endl;
+    cout << "the factorial of 5 is :" << factorial(5) << endl;
+    return 0;
+
+
+}
